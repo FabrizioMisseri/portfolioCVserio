1216e2ce934d2fbe4c4e2d3cd290158da67e9b3d
FabrizioMisseri <misserifabrizio@gmail.com>
2023-06-24 11:18:13 +0200

    add skills (SingleSkill-component)

---
+++ src/mini-components/SingleSkill.vue
@@ -0,0 +1,86 @@
+<script>
+
+export default {
+    name: 'SingleSkill',
+
+    props: {
+        skill: Object,
+    },
+
+    data() {
+        return {
+            titleFlag: false,
+        }
+    },
+
+    methods: {
+        getImage(path) {
+            return new URL(path, import.meta.url).href;
+        },
+
+    },
+}
+</script>
+
+<template>
+    <div class="item p-5 d-flex justify-content-center">
+        <figure class="scroll-container">
+            <img :src="getImage(skill.logoPath)" alt="">
+            <div class="title fs-4 scroll-content">
+                {{ skill.title }}
+            </div>
+        </figure>
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/colors.scss" as *;
+
+.item {
+
+    figure {
+        position: relative;
+
+        img {
+            min-width: 125px;
+            max-width: 125px;
+        }
+
+        .title {
+            display: none;
+            position: absolute;
+            z-index: 1;
+            top: 45%;
+            left: 150px;
+        }
+    }
+
+    &:hover .title {
+        display: block;
+    }
+
+    .scroll-container {
+        white-space: nowrap;
+    }
+
+    .scroll-content {
+        display: inline-block;
+        animation: scroll-right .3s linear;
+    }
+
+    @keyframes scroll-right {
+        0% {
+            transform: translateX(-100%);
+        }
+
+        33% {
+            transform: translateX(-50%);
+        }
+
+        100% {
+            transform: translateX(0%);
+        }
+    }
+}
+</style>
--- store.js
+++ store.js
@@ -23,6 +23,53 @@ export const store = reactive({
             name: "Contacts",
             link: "contacts"
         },
-    ]
+    ],
+
+    logosArray: [
+        {
+            title: 'HTML',
+            logoPath: '/public/logos/logo-html.png',
+        },
+        {
+            title: 'BootStrap',
+            logoPath: '/public/logos/logo-bootstrap.png',
+        },
+        {
+            title: 'JavaScript',
+            logoPath: '/public/logos/logo-javascript.png',
+        },
+        {
+            title: 'VueJS',
+            logoPath: '/public/logos/logo-vue.png',
+        },
+        {
+            title: 'NPM',
+            logoPath: '/public/logos/logo-npm.png',
+        },
+        {
+            title: 'MySQL',
+            logoPath: '/public/logos/logo-mysql.png',
+        },
+        {
+            title: 'PHP',
+            logoPath: '/public/logos/logo-php.png',
+        },
+        {
+            title: 'Laravel',
+            logoPath: '/public/logos/logo-laravel.png',
+        },
+        {
+            title: 'Java',
+            logoPath: '/public/logos/logo-java.png',
+        },
+        {
+            title: 'Spring',
+            logoPath: '/public/logos/logo-spring.png',
+        },
+        {
+            title: 'CSS',
+            logoPath: '/public/logos/logo-css.png',
+        },
+    ],
 
 });
