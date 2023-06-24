dfd427d42d496e3909f8da4fa21a1600aa39de88
FabrizioMisseri <misserifabrizio@gmail.com>
2023-06-24 11:58:30 +0200

    ***

--- store.js
+++ store.js
@@ -28,47 +28,47 @@ export const store = reactive({
     logosArray: [
         {
             title: 'HTML',
-            logoPath: '/assets/public/logos/logo-html.png',
+            logoPath: '/public/logos/logo-html.png',
         },
         {
             title: 'BootStrap',
-            logoPath: '/assets/public/logos/logo-bootstrap.png',
+            logoPath: '/public/logos/logo-bootstrap.png',
         },
         {
             title: 'JavaScript',
-            logoPath: '/assets/public/logos/logo-javascript.png',
+            logoPath: '/public/logos/logo-javascript.png',
         },
         {
             title: 'VueJS',
-            logoPath: '/assets/public/logos/logo-vue.png',
+            logoPath: '/public/logos/logo-vue.png',
         },
         {
             title: 'NPM',
-            logoPath: '/assets/public/logos/logo-npm.png',
+            logoPath: '/public/logos/logo-npm.png',
         },
         {
             title: 'MySQL',
-            logoPath: '/assets/public/logos/logo-mysql.png',
+            logoPath: '/public/logos/logo-mysql.png',
         },
         {
             title: 'PHP',
-            logoPath: '/assets/public/logos/logo-php.png',
+            logoPath: '/public/logos/logo-php.png',
         },
         {
             title: 'Laravel',
-            logoPath: '/assets/public/logos/logo-laravel.png',
+            logoPath: '/public/logos/logo-laravel.png',
         },
         {
             title: 'Java',
-            logoPath: '/assets/public/logos/logo-java.png',
+            logoPath: '/public/logos/logo-java.png',
         },
         {
             title: 'Spring',
-            logoPath: '/assets/public/logos/logo-spring.png',
+            logoPath: '/public/logos/logo-spring.png',
         },
         {
             title: 'CSS',
-            logoPath: '/assets/public/logos/logo-css.png',
+            logoPath: '/public/logos/logo-css.png',
         },
     ],
 
