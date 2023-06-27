ae04b5ce12def8642038e502084f86695bcb8478
FabrizioMisseri <misserifabrizio@gmail.com>
2023-06-27 11:13:15 +0200

    fix contacts-style *

--- src/mini-components/ContactLinks.vue
+++ src/mini-components/ContactLinks.vue
@@ -19,39 +19,39 @@ export default {
     <!-- LINKS -->
     <section class="container fs-2">
         <div class="row d-flex justify-content-center">
-            <div class="col-7 rounded-4">
-                <a class="px-5 py-2" href="https://www.linkedin.com/in/fabrizio-misseri/">
-                    <i class="fa-brands fa-linkedin"></i>
-                    <span class="ms-3">Linkedin</span>
-                </a>
-            </div>
 
-            <div class="col-7 rounded-4">
-                <a class="px-5 py-2" href="https://github.com/FabrizioMisseri">
-                    <i class="fa-brands fa-square-github"></i>
-                    <span class="ms-3">GitHub</span>
-                </a>
-            </div>
+            <a class="col-7 rounded-4 px-5 py-2" href="https://www.linkedin.com/in/fabrizio-misseri/">
+                <i class="me-3 fa-brands fa-linkedin"></i>
+                Linkedin
+            </a>
 
-            <div class="col-7 rounded-4">
-                <a class="px-5 py-2" href="https://fabrizio-misseri-links.netlify.app/">
-                    <i class="fa-solid fa-square-up-right"></i>
-                    <span class="ms-3">MyLinks</span>
-                </a>
-            </div>
 
-            <div class="col-7 rounded-4">
-                <a class="px-5 py-2" :href="`${pathCV}`" download>
-                    <i class="fa-solid fa-file-pdf"></i>
-                    <span class="ms-3">Curriculum</span>
-                </a>
-            </div>
-            <div class="col-7 rounded-4">
-                <a class="px-5 py-2" @click="sendMail()">
-                    <i class="fa-solid fa-envelope"></i>
-                    <span class="ms-3">misserifabrizio@gmail.com</span>
-                </a>
-            </div>
+
+            <a class="col-7 rounded-4 px-5 py-2" href="https://github.com/FabrizioMisseri">
+                <i class="me-3 fa-brands fa-square-github"></i>
+                GitHub
+            </a>
+
+
+
+            <a class="col-7 rounded-4 px-5 py-2" href="https://fabrizio-misseri-links.netlify.app/">
+                <i class="me-3 fa-solid fa-square-up-right"></i>
+                MyLinks
+            </a>
+
+
+
+            <a class="col-7 rounded-4 px-5 py-2" :href="`${pathCV}`" download>
+                <i class="me-3 fa-solid fa-file-pdf"></i>
+                Curriculum
+            </a>
+
+
+            <a class="col-7 rounded-4 px-5 py-2" @click="sendMail()">
+                <i class="me-3 fa-solid fa-envelope"></i>
+                misserifabrizio@gmail.com
+            </a>
+
         </div>
     </section>
     <!-- / LINKS -->
@@ -62,9 +62,15 @@ export default {
 
 section {
 
-    .col-7:hover {
+    a:hover {
+        display: inline-block;
         background-color: $orange;
+        font-size: inherit;
+    }
+
+    .col-7 {
         cursor: pointer;
     }
+
 }
 </style>
